c5627f61782fc845baced42f0521d47918b8b213
Hazel-1212 <achievegoal9@gmail.com>
2025-06-07 22:16:54 +0800

    Update blood

--- ui.py
+++ ui.py
@@ -2,5 +2,5 @@ import pygame
 
 def hud(screen,score):
     font = pygame.font.SysFont(None, 36)
-    score_text = font.render(f"Score: {score}", True, (0, 0, 0))
+    score_text = font.render(f"Blood: {score}", True, (255, 255, 0))
     screen.blit(score_text, (10, 10))  # 顯示在畫面左上角
